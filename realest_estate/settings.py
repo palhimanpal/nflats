899096f29995f91920327ce39811d1a7024d4252
palhimanpal <palhimanpal786@gmail.com>
2022-02-17 21:54:59 +0530

    [update] settings.py

--- realest_estate/settings.py
+++ realest_estate/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'palhimanpal786@gmail.com'
-EMAIL_HOST_PASSWORD = 'h7588815973c'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Password validation
